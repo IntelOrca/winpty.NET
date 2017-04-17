6cabed6efe6e32acc76e1bcfdb451dc1e5d06ae0
Ted John <ted@brambles.org>
2017-04-17 17:34:28 +0100

    Add initial sources and nuspec

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # winpty.NET
-.NET wrapper for winpty
+A .NET wrapper for [winpty](https://github.com/rprichard/winpty).
+
+## Copyright
+winpty.NET and winpty are both distributed under the MIT license.
---
+++ winpty.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("winpty.NET")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("winpty.NET")]
+[assembly: AssemblyCopyright("Copyright © Ted John 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("f29a07ee-9775-41b7-a249-388ea360fa0c")]
+
+[assembly: AssemblyVersion("0.4.2.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
